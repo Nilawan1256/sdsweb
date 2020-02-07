57af415e59dcf0501876d74e493308c5badcd20c
Kan Seummek <kanz.kingz@gmail.com>
2020-02-07 14:27:40 +0700

    dashboard update

--- app/controllers/index-controller.js
+++ app/controllers/index-controller.js
@@ -1,5 +1,7 @@
 var async = require("async");
 var _ = require("lodash");
+var moment = require('moment');
+var models = require("../models");
 
 var exports = module.exports = {}
 
@@ -8,7 +10,46 @@ exports.index = async function(req, res, next){
 		// const db = req.app.db;
 		// const _user = await db.user.findOne({ id:1 });
 		// console.log(_user);
-		res.render('index', { title: 'เสถียรธรรมสถาน ระบบจัดการข้อมูลผู้ร่วมบุญ', menu_left:'dashboard', page_title:'', data:null });
+		const data = [];
+
+		const Datenow = moment(Date.now()).format('YYYY/MM/DD');
+		const sql_order_pepole_total = "select COUNT(id) as total from sdsweb.order where create_date = '" + Datenow + "'";
+		await (
+			models.sequelize.query(sql_order_pepole_total, { type: models.sequelize.QueryTypes.SELECT })
+			.then(_data => {
+				data.order_pepole_total = _data;
+				// console.log("data.order_pepole_total = " + JSON.stringify(_data));
+			})
+			.catch(err => {
+				console.log("ค้นหาข้อมูลไม่สำเร็จ " + err);
+			})
+		);
+
+		// const sql_order_pepole_total = "select COUNT(id) as total from sdsweb.order where create_date = '" + Datenow + "'";
+		// await (
+		// 	models.sequelize.query(sql_order_pepole_total, { type: models.sequelize.QueryTypes.SELECT })
+		// 	.then(_data => {
+		// 		data.order_pepole_total = _data;
+		// 		// console.log("data.order_pepole_total = " + JSON.stringify(_data));
+		// 	})
+		// 	.catch(err => {
+		// 		console.log("ค้นหาข้อมูลไม่สำเร็จ " + err);
+		// 	})
+		// );
+
+		const sql_order = "select id, donor_id, (select text from lov where lov.id = `order`.lov_service_point_id) as lov_service_point_id, order_name, total, product_group_id, receipt_file, payment_period, (select text from lov where lov.id = `order`.lov_payment_status) as lov_payment_status, `comment`, create_by, DATE_FORMAT(create_date, \"%d/%m/%Y\") as create_date, (select username from user where user.id = `order`.create_by) as create_by,(select username from user where user.id = `order`.update_by) as update_by, DATE_FORMAT(update_date, \"%d/%m/%Y\") as update_date from sdsweb.order";
+		await (
+			models.sequelize.query(sql_order, { type: models.sequelize.QueryTypes.SELECT })
+			.then(_data => {
+				data.order = _data;
+				// console.log("data.order = " + JSON.stringify(_data));
+			})
+			.catch(err => {
+				console.log("ค้นหาข้อมูลไม่สำเร็จ " + err);
+			})
+		);
+
+		res.render('index', { title: 'เสถียรธรรมสถาน ระบบจัดการข้อมูลผู้ร่วมบุญ', menu_left:'dashboard', page_title:'', data:data });
 	}
 	catch(err){
 		next();
